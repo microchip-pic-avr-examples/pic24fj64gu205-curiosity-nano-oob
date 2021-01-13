9eb481170fda83ffffd4da23690d3bd58f3e2fc3
David Flowers <David.Flowers@microchip.com>
2021-01-13 15:15:50 -0500

    merging differences.

--- pic24fj64gu205-curiosity-nano-oob.X/mcc_generated_files/mcc.h
+++ pic24fj64gu205-curiosity-nano-oob.X/mcc_generated_files/mcc.h
@@ -51,9 +51,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "usb/usb.h"
 #include "interrupt_manager.h"
 #include "traps.h"
+#include "usb/usb.h"
 
 #warning "This file will be removed in future MCC releases. Use system.h instead."
 
--- pic24fj64gu205-curiosity-nano-oob.X/mcc_generated_files/pin_manager.c
+++ pic24fj64gu205-curiosity-nano-oob.X/mcc_generated_files/pin_manager.c
@@ -15,11 +15,11 @@
   @Description:
     This source file provides implementations for PIN MANAGER.
     Generation Information :
-        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.167.0
+        Product Revision  :  PIC24 / dsPIC33 / PIC32MM MCUs - 1.170.0
         Device            :  PIC24FJ64GU205
     The generated drivers are tested against the following:
-        Compiler          :  XC16 v1.50
-        MPLAB 	          :  MPLAB X v5.35
+        Compiler          :  XC16 v1.61
+        MPLAB 	          :  MPLAB X v5.45
 */
 
 /*
